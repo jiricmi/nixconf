e0ca6540d1e1aa0f1f761a0fa71d4c17846ea355
jiricmi <accounts@mjiricek.eu>
2024-09-02 11:33:29 +0200

    basic nvim setup

--- modules/features/nixvim/default.nix
+++ modules/features/nixvim/default.nix
@@ -3,6 +3,32 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-  };
+    
+    options = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
 
+    colorschemes.gruvbox.enable = true;
+
+    plugins.lualine.enable = true;
+    
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        pyright.enable = true;
+	tsserver.enable = true;
+	lua-ls = {
+	  enable = true;
+          settings.telemetry.enable = false;
+	};
+      };
+    };
+
+    plugins.nvim-cmp = {
+      enable = true;
+      autoEnableSources = true;
+    };
+  };
 }
