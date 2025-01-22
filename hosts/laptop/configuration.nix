3ba2a1fd9fb472013752ca87dedbc2e29eed0178
jiricmi <accounts@mjiricek.eu>
2025-01-22 11:14:35 +0100

    d

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -20,7 +20,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’
   users.users.jiricmi = {
     isNormalUser = true;
-    extraGroups = ["networkmanager" "wheel" "audio" "docker" "libvirtd" ];
+    extraGroups = ["networkmanager" "wheel" "audio" "docker" "libvirtd" "dialout"];
   };
 
   home-manager = {
